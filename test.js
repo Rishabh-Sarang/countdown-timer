1a62643162e7bbb8f291fa13863f6752d28073d6
Rishabh_Sarang <rishabhsarang200@gmail.com>
2023-08-19 17:02:41 +0530

    test

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-const poerosn = {
-    name1 : 'rishabh',
-    age : 10,
-    asdf: 4,
-    qwer : 12
-}
-var arr = []
-for (const key in poerosn) {
-    console.log(`${poerosn[key]}`);
-    asdjhasdaasdf
-}
+test
