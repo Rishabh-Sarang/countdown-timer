f374f87a4e2d6b08a23ee669c2d69536e5524064
Rishabh_Sarang <rishabhsarang200@gmail.com>
2023-08-25 15:34:09 +0530

    asdf

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName = "rishabh";
+var courseName = 'abc';
+modeOfPayment = prompt("mode of payment");
+alert(firstName + " succsesfully bought " + courseName + " using mode of payment " + modeOfPayment);
+
