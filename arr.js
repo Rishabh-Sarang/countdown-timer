06a6596a29ff41af6b6c1ac1c4f3e147993c814c
Rishabh_Sarang <rishabhsarang200@gmail.com>
2023-08-30 16:08:46 +0530

    "asdf"

--- arr.js
+++ arr.js
@@ -4,11 +4,11 @@ arr.shift();
 arr.includes(4,2);
  
 const array = [{name: 'asdf',id: 1,name: 'qwer', id: 2}]
-//console.log(array.find(function(element){return element.id==2}))
-//console.log(array.find(element => {return element.id==1}))
+console.log(array.find(function(element){return element.id==2}))
+console.log(array.find(element => {return element.id==1}))
 
-//console.log(arr.concat(array));
+console.log(arr.concat(array));
 
-//console.log(arr.join(' -6.9- '))
+console.log(arr.join(' -6.9- '))
 
 console.log(array.filter(element=>{return element.id>1}))
